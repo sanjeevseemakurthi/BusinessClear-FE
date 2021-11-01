f5a8e6b17d22e125d3095b67b2bd60c395d8d827
sanjeevseemakurthi <sanju.seemakurthi8@gmail.com>
2021-11-01 23:16:20 +0530

    1-11-2021

--- src/app/dashbord/dashbord.module.ts
+++ src/app/dashbord/dashbord.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { DashbordRoutingModule } from './dashbord-routing.module';
 import { SidenavComponent } from './sidenav/sidenav.component';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [SidenavComponent],
   imports: [
     CommonModule,
-    DashbordRoutingModule
+    DashbordRoutingModule,
+    MatSidenavModule
   ]
 })
 export class DashbordModule { }
--- src/app/dashbord/sidenav/sidenav.component.ts
+++ src/app/dashbord/sidenav/sidenav.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
-
-  constructor() { }
+  islargescreen = false;
+  showFiller = false;
+  sidenavtoggle = true
+  constructor( breakpointObserver: BreakpointObserver) {
+    breakpointObserver
+    .observe([Breakpoints.Large])
+    .pipe()
+    .subscribe((result) => {
+      this.islargescreen = result.matches;
+    });
+   }
 
   ngOnInit(): void {
   }
-
+sidenavtogglemethod()
+{
+  this.sidenavtoggle = ! this.sidenavtoggle;
+}
 }
