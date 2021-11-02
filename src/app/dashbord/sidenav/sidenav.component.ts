5cd9f861dcbd2a19cf17f6c868a6231680bde1a8
sanjeevseemakurthi <sanju.seemakurthi8@gmail.com>
2021-11-02 21:58:01 +0530

    nav bar changes

--- src/app/dashbord/sidenav/sidenav.component.ts
+++ src/app/dashbord/sidenav/sidenav.component.ts
@@ -8,7 +8,24 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 export class SidenavComponent implements OnInit {
   islargescreen = false;
   showFiller = false;
-  sidenavtoggle = true
+  sidenavtoggle = true;
+  imageurl = '';
+  menuitems = [
+    {
+      icon :'fa-home',
+      name : 'Home'
+    },
+    {
+      icon :' fa-chart-bar',
+      name : 'Analysis'
+    },{
+      icon :'fa-chart-pie',
+      name : 'Stocks/Sales'
+    },{
+      icon :'fa-money-bill',
+      name : 'Lent'
+    },
+  ]
   constructor( breakpointObserver: BreakpointObserver) {
     breakpointObserver
     .observe([Breakpoints.Large])
